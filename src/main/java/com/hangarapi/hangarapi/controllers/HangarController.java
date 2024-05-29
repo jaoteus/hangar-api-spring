a400d5aa864011fd09bbb90c262dd609242a82c8
Mateus <mateusff2323@gmail.com>
2024-05-28 21:02:43 -0300

    added method to add aircrafts in HangarController class

--- src/main/java/com/hangarapi/hangarapi/controllers/HangarController.java
+++ src/main/java/com/hangarapi/hangarapi/controllers/HangarController.java
@@ -7,7 +7,9 @@ import com.hangarapi.hangarapi.repositories.HangarRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -19,12 +21,12 @@ public class HangarController {
     @Autowired
     private HangarRepository hangarRepository;
 
-//    @Autowired
-//    private AircraftRepository aircraftRepository;
+    @Autowired
+    private AircraftRepository aircraftRepository;
 
     @GetMapping
-    public List<Hangar> getAllHangars() {
-        return hangarRepository.findAll();
+    public ResponseEntity<List<Hangar>> getAllHangars() {
+        return ResponseEntity.ok().body(hangarRepository.findAll());
     }
 
     @GetMapping("/{id}")
@@ -34,12 +36,26 @@ public class HangarController {
     }
 
     @PostMapping
-    public Hangar createHangar(@RequestBody Hangar hangar) {
-        return hangarRepository.save(hangar);
+    public ResponseEntity<Hangar> createHangar(@RequestBody Hangar hangar) {
+        hangarRepository.save(hangar);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(hangar.getId()).toUri();
+        return ResponseEntity.created(uri).body(hangar);
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<Hangar> updateHangar(@PathVariable Long id, @RequestBody Hangar hangarDetails) {
+    public ResponseEntity<Hangar> updateAllHangarData(@PathVariable Long id, @RequestBody Hangar hangarDetails) {
+        Optional<Hangar> hangar = hangarRepository.findById(id);
+        if (hangar.isPresent()) {
+            Hangar updatedHangar = hangar.get();
+            updatedHangar.setAirportRegistration(hangarDetails.getAirportRegistration());
+            return ResponseEntity.ok(hangarRepository.save(updatedHangar));
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<Hangar> updateSomeHangarData(@PathVariable Long id, @RequestBody Hangar hangarDetails) {
         Optional<Hangar> hangar = hangarRepository.findById(id);
         if (hangar.isPresent()) {
             Hangar updatedHangar = hangar.get();
@@ -61,53 +77,18 @@ public class HangarController {
         }
     }
 
-//    // Aircraft Endpoints
-//
-//    @GetMapping("/{id}/aircrafts")
-//    public ResponseEntity<List<Aircraft>> getAircraftsByHangarId(@PathVariable Long id) {
-//        Optional<Hangar> hangar = hangarRepository.findById(id);
-//        return hangar.map(h -> ResponseEntity.ok(h.getAircrafts())).orElseGet(() -> ResponseEntity.notFound().build());
-//    }
-//
-//    @PostMapping("/{id}/aircrafts")
-//    public ResponseEntity<Hangar> addAircraftToHangar(@PathVariable Long id, @RequestBody Aircraft aircraft) {
-//        if (aircraftRepository.findByAircraftRegistration(aircraft.getAircraftRegistration()).isPresent()) {
-//            return ResponseEntity.badRequest().body(null);
-//        }
-//
-//        Hangar hangar = hangarRepository.findById(id).orElseThrow(() -> new RuntimeException("Hangar not found"));
-//        hangar.getAircrafts().add(aircraft);
-//        return ResponseEntity.ok(hangarRepository.save(hangar));
-//    }
-//
-//    @PutMapping("/aircrafts/{aircraftId}")
-//    public ResponseEntity<Aircraft> updateAircraft(@PathVariable Long aircraftId, @RequestBody Aircraft aircraftDetails) {
-//        Optional<Aircraft> aircraft = aircraftRepository.findById(aircraftId);
-//        if (aircraft.isPresent()) {
-//            Aircraft updatedAircraft = aircraft.get();
-//            updatedAircraft.setAircraftStatus(aircraftDetails.getAircraftStatus());
-//            updatedAircraft.setOperator(aircraftDetails.getOperator());
-//            updatedAircraft.setSeatingCapacity(aircraftDetails.getSeatingCapacity());
-//            updatedAircraft.setModel(aircraftDetails.getModel());
-//            updatedAircraft.setAircraftRegistration(aircraftDetails.getAircraftRegistration());
-//
-//            if (aircraftRepository.findByAircraftRegistration(updatedAircraft.getAircraftRegistration()).isPresent()) {
-//                return ResponseEntity.badRequest().body(null);
-//            }
-//            return ResponseEntity.ok(aircraftRepository.save(updatedAircraft));
-//        } else {
-//            return ResponseEntity.notFound().build();
-//        }
-//    }
-//
-//    @DeleteMapping("/aircrafts/{aircraftId}")
-//    public ResponseEntity<Void> deleteAircraft(@PathVariable Long aircraftId) {
-//        Optional<Aircraft> aircraft = aircraftRepository.findById(aircraftId);
-//        if (aircraft.isPresent()) {
-//            aircraftRepository.delete(aircraft.get());
-//            return ResponseEntity.noContent().build();
-//        } else {
-//            return ResponseEntity.notFound().build();
-//        }
-//    }
+    @PostMapping("/{hangarId}/aircrafts/{aircraftId}")
+    public ResponseEntity<Aircraft> addAircraftInHangar(@PathVariable Long hangarId, @PathVariable Long aircraftId) {
+        Optional<Aircraft> optionalAircraft = aircraftRepository.findById(aircraftId);
+        Optional<Hangar> optionalHangar = hangarRepository.findById(hangarId);
+        if (optionalAircraft.isPresent() && optionalHangar.isPresent()) {
+            Hangar hangar = optionalHangar.get();
+            Aircraft aircraft = optionalAircraft.get();
+            hangar.addAircraft(aircraft);
+            hangarRepository.save(hangar);
+            return ResponseEntity.ok().body(aircraft);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
